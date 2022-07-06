41458fcf43d2ff5a87999d43507ed7c30ad09a15
barisvdeprodesia <baris.vdeprodesia@gmail.com>
2022-07-06 21:20:08 +0300

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class HomePage extends StatelessWidget {
                         "assets/images/Profil Fotosu 5 büyük daire.png",
                       )),
                   Text(
-                    "Barış Aslan",
+                    "Ben Barış Aslan",
                     style: GoogleFonts.montserrat(
                       fontSize: 25,
                       fontWeight: FontWeight.bold,
